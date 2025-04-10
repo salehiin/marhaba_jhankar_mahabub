8eb4b52aeb5faa2966300507669fa3f1b0d696df
salehiin <salehin.ay@gmail.com>
2025-04-10 16:19:43 +0600

    first commit after complete variable chapter

---
+++ variable.js
@@ -0,0 +1,59 @@
+// practice - p47
+
+// 1
+const income = 100;
+const expenses = 40;
+const remaining = income - expenses;
+console.log(remaining);
+
+// 2
+const quantity = 10;
+const eachTimeUse = 2;
+const timesCanUse = quantity / eachTimeUse;
+console.log(timesCanUse);
+
+// 3
+const amountHave = 60;
+const eachTimeExpend = 15;
+const timesCanExpend = amountHave / eachTimeExpend;
+const remainingAmount = amountHave % eachTimeExpend;
+console.log(timesCanExpend, remainingAmount);
+
+// 4
+const hello = 'Hello';
+const world = 'World';
+const afterAdd = hello + world;
+console.log(afterAdd);
+
+// 5
+const rice = 153;
+const pulse = 261;
+const totalThings = rice + pulse;
+console.log(totalThings);
+
+// 6
+const totalAmount = 500;
+const expenseEachTime = 75;
+const timesCanExpense = totalAmount / expenseEachTime;
+const remainingTotal = totalAmount % expenseEachTime;
+console.log(timesCanExpense, remainingTotal);
+
+// 7
+console.log(8 / 3);
+console.log(8 % 3);
+
+// 8
+console.log(50 % 9);
+
+// 9
+const bangla = 'Bangla';
+const desh = 'desh';
+const country = bangla + desh;
+console.log(country);
+
+// 10
+const eachDayConsume = 4;
+const totalRice = 480;
+const daysCanConsume = 480 / 4;
+const monthsCanConsume = daysCanConsume / 30;
+console.log(`${daysCanConsume} days, ${monthsCanConsume} months`);
