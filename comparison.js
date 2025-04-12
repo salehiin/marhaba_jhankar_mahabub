0702b0a500fe429d3054564719a9cd7f4a537e97
salehiin <salehin.ay@gmail.com>
2025-04-12 15:46:48 +0600

    comparison done

---
+++ comparison.js
@@ -0,0 +1,39 @@
+console.log(120 === 'tomato');
+console.log(120 !== 'tomato');
+
+// practice - p73
+myAmount = 50;
+friendsAmount = '50';
+console.log(myAmount == friendsAmount)
+console.log(myAmount === friendsAmount)
+
+
+// 2
+myNotebook = "JavaScript";
+myJournal = 'JavaScript';
+console.log(myNotebook == myJournal);
+console.log(myNotebook === myJournal);
+
+// 3
+console.log(25 == "25");
+console.log(25 === "25");
+
+// 4
+console.log("apple" == "apple");
+console.log("apple" === "apple");
+
+// 5
+console.log("test" == "TEST");
+console.log("test" === "TEST");
+
+// 6
+variableX =15;
+variableY = "20";
+console.log(`Variable: ${variableX != variableY}`);
+console.log(variableX !== variableY);
+
+// 7
+const variableA = 'hello';
+const variableB = 'Hello';
+console.log(`Variable: ${variableA == variableB}`);
+console.log(variableA === variableB);
